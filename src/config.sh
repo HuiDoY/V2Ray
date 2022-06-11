35d80a1279ac37a9b56895b3d4d4df7695497cf5
Corི༅ ҉ <56605091+HuiDoY@users.noreply.github.com>
2022-06-11 19:08:43 +0800

    .

--- src/config.sh
+++ src/config.sh
@@ -1,14 +1,6 @@
 
 # vmess
 _load vmess-config.sh
-# ss
-_load ss-config.sh
-
-# socks
-_load socks-config.sh
-
-# mtproto
-_load mtproto-config.sh
 
 # ban domain
 _load ban_xx.sh
@@ -18,6 +10,18 @@ _load ban_bt.sh
 
 # ban ad
 _load ban_ad.sh
+
+# custom rules
+_load custom_rules.sh
+
+# ss
+_load ss-config.sh
+
+# socks
+_load socks-config.sh
+
+# mtproto
+_load mtproto-config.sh
 
 # custom config
 _load custom_config.sh
